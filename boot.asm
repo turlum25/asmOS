36ed1b79a60346c6a75fdba18cfa1bfad4233dd2
burntsierra <raihan.shanto14@gmail.com>
2026-09-23 23:49:52 +0600

    Fix bootloader stuff. Add note too.

--- boot.asm
+++ boot.asm
@@ -2,9 +2,13 @@
 [org 0x7C00]
 
 start:
+    mov [boot_drive], dl
+
     xor ax, ax
     mov ds, ax
     mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
 
     mov ah, 0x00
     mov al, 0x03
@@ -14,22 +18,27 @@ start:
     call print_string_16
 
     mov ah, 0x02
-    mov al, 2
+    mov al, 5 ; Note: This is the amount of sectors bootloader will read.
     mov ch, 0
     mov dh, 0
     mov cl, 2
+    mov dl, [boot_drive]
     mov bx, 0x8000
     int 0x13
     jc disk_crash
 
     cli
     lgdt [gdt_descriptor]
+    
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
+
     jmp 0x08:0x8000
 
 disk_crash:
+    mov si, msg_error
+    call print_string_16
     jmp $
 
 print_string_16:
@@ -43,13 +52,16 @@ print_string_16:
 .done:
     ret          
 
+boot_drive db 0
 msg_ebios db "boot", 0x0D, 0x0A, 0
+msg_error db "Disk error", 0x0D, 0x0A, 0
 
 gdt_start:
 gdt_null: dd 0x0, 0x0
 gdt_code: dw 0xffff, 0x0, 0x9a00, 0xcf
 gdt_data: dw 0xffff, 0x0, 0x9200, 0xcf
 gdt_end:
+
 gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
